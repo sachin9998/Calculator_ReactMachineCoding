6ac613e31cf5f406bb5b2d5ae058324ce78a24ae
Sachin Alam <sachinalam4216@gmail.com>
2025-02-11 11:04:52 +0530

    Calculator app ready

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+
+const App = () => {
+  const [input, setInput] = useState("");
+
+  const arr = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "+",
+    "-",
+    "/",
+    "*",
+    "=",
+    "C",
+    ".",
+  ];
+
+  const handleClick = (e) => {
+    const id = e.target.id;
+
+    if (e.target.id === "C") {
+      setInput("");
+    } else if (e.target.id === "=") {
+      // Calculate result
+      handleSubmit();
+    } else {
+      setInput((prev) => prev + id);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e?.preventDefault();
+
+    try {
+      const ans = eval(input);
+      setInput(ans);
+    } catch (err) {
+      alert("Invalid Inputs");
+    }
+  };
+
+  return (
+    <div className="container mx-auto">
+      <h1 className="my-4 text-3xl text-center">Calculator App</h1>
+
+      <div className="my-4">
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              className="w-full border border-slate-200 rounded p-2"
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </form>
+        </div>
+
+        <div
+          className="mt-5 h-[50vh] grid grid-cols-3 gap-1"
+          onClick={handleClick}
+        >
+          {arr.map((item, idx) => (
+            <button
+              className="bg-orange-400 text-white font-bold rounded hover:cursor-pointer"
+              id={item}
+              key={idx}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
